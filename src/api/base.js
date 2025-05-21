bc45d917fc12b13c837718c3a3d87731ef8f2e5f
Sahis <sahisneupane23@gmail.com>
2025-05-21 13:49:43 -0700

    feat: add Vercel configuration for URL rewrites

--- src/api/base.js
+++ src/api/base.js
@@ -7,6 +7,7 @@ export const axios = axiosModule.create({
 });
 
 export async function request(config) {
+  console.log(baseUrl);
   const headers = config.headers || {};
 
   if (config.accessToken) {
